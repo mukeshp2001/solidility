f3dd481153813f340e8381c6704ac20f29c6b8f5
mukeshp2001 <146801985+mukeshp2001@users.noreply.github.com>
2023-10-05 10:41:42 +0530

    Add files via upload

---
+++ pure.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity>=0.5.0<0.9.0;
+contract local
+{
+   uint age=30;
+   function getter()public pure returns (uint)
+   {
+      uint roll=102;
+      return roll;
+   }
+   
+   }
