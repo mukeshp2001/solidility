24b0f7b1d61737e44ec5181b0fcdb0246a01d729
mukeshp2001 <146801985+mukeshp2001@users.noreply.github.com>
2023-10-05 13:50:33 +0530

    Add files via upload

---
+++ for_loop.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity>=0.5.0<0.9.0;
+
+contract array
+{
+   uint[3] public arr;
+   uint public count;
+   function loop() public{
+    for(uint i=count;i<arr.length;i++)
+    {
+        arr[count]=count;
+        count ++;
+    }
+   }
+}
