305e8ff1d68cacd35999221657712cdc277be3d3
mukeshp2001 <146801985+mukeshp2001@users.noreply.github.com>
2023-10-05 11:17:48 +0530

    Add files via upload

---
+++ overflowcase.sol
@@ -0,0 +1,11 @@
+pragma solidity  0.5.0;
+
+// SPDX-License-Identifier:GPL-3.0
+contract local
+{
+    uint public money=255;
+    function setter ()public 
+    {
+        money=money+1;
+    }
+}    
